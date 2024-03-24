154b9bef760de7a63e1a74622c6ea9d4e1da6fa2
Nikita Melnikov <114857730+n1klze@users.noreply.github.com>
2024-03-25 03:16:01 +0700

    enum Filters added to git.

---
+++ src/main/java/ru/nsu/ccfit/melnikov/model/Filters.java
@@ -0,0 +1,34 @@
+package ru.nsu.ccfit.melnikov.model;
+
+import lombok.Getter;
+
+public enum Filters {
+    ROTATION("Rotation", "R"),
+    FLOYD_STEINBERG_DITHERING("Floyd-Steinberg dithering", "FSD"),
+    ORDERED_DITHERING("Ordered dithering", "OD"),
+    BLUR("Blur", "B"),
+    GRAYSCALE("Grayscale", "GS"),
+    WATERCOLOR("Watercolor effect", "WE"),
+    ZOOM("Zoom", "Z"),
+    NORMAL_MAP("Normal mapping", "N"),
+    EMBOSSING("Embossing", "E"),
+    SHARPNESS("Sharpness", "S"),
+    SOBEL("Sobel operator", "So"),
+    ROBERTS("Roberts operator", "Ro"),
+    GAMMA("Gamma correction", "G"),
+    INVERSE("Inverse", "I");
+
+    private final String name;
+    @Getter
+    private final String pict;
+
+    Filters(String name, String pict) {
+        this.name = name;
+        this.pict = pict;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
